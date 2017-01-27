cb6dcb6ddaae75adff886b48ad6478df8f13fd95
SuperHaker <amrits3624@gmail.com>
2017-01-28 01:37:28 +0530

    SignOut added

--- app/src/main/java/com/example/android/wednesday/MainActivity.java
+++ app/src/main/java/com/example/android/wednesday/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.wednesday;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -8,9 +9,12 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.android.wednesday.fragments.TabOneFragment;
 import com.example.android.wednesday.fragments.TabTwoFragment;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -79,5 +83,26 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+
+            case R.id.signOut:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), LogInActivity.class));
+                finish();
+                return true;
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
